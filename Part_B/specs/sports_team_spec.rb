cd02150276358a7374d57f2cf08010b882489bae
Kyle <kirvine87@gmail.com>
2019-07-22 17:56:39 +0100

    completed check name and team result function and test

--- Part_B/specs/sports_team_spec.rb
+++ Part_B/specs/sports_team_spec.rb
@@ -5,7 +5,7 @@ require_relative('../sports_team.rb')
 class TestSportsTeam < MiniTest::Test
 
 def setup()
-  @team = SportsTeam.new("Storm", ["Ben", "Adam", "Joe", "Sarah"], "Coach")
+  @team = SportsTeam.new("Storm", ["Ben", "Adam", "Joe", "Sarah"], "Coach", 0)
 end
 
 def test_get_team_name()
@@ -28,6 +28,19 @@ end
 def test_add_new_player()
   @team.add_new_player("Hannah")
   assert_equal(5, @team.players.count())
+end
+
+def test_check_name()
+  assert_equal(true, @team.check_name("Ben"))
+end
+
+def test_get_points()
+  assert_equal(0, @team.points)
+end
+
+def test_team_result()
+  @team.team_result("Won")
+  assert_equal(3, @team.points())
 end
 
 end
--- Part_B/sports_team.rb
+++ Part_B/sports_team.rb
@@ -1,16 +1,30 @@
 class SportsTeam
 
-attr_reader :team_name, :players
+attr_reader :team_name, :players, :points
 attr_accessor :coach
-def initialize(team_name,players,coach)
+def initialize(team_name,players,coach,points)
   @team_name = team_name
   @players = players
   @coach = coach
+  @points = points
 end
 
 
 def add_new_player(player)
   @players << player
+end
+
+def check_name(name)
+  @players.include?(name)
+end
+
+def team_result(result)
+
+  if(result == "Won") then @points += 3
+  elsif(result == "Draw") then @points += 0
+  else @points -= 3
+  end
+
 end
 
 end

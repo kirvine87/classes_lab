07e97f2b0067dc00ebd0921372f8e2b55e4357ed
Kyle <kirvine87@gmail.com>
2019-07-22 22:19:15 +0100

    completed gets book function and tes

---
+++ Extension/library.rb
@@ -0,0 +1,11 @@
+class Library
+
+  def initialize(books)
+    @books = books
+  end
+
+def get_books()
+  return @books
+end
+
+end
--- Extension/specs/library_spec.rb
+++ Extension/specs/library_spec.rb
@@ -1,1 +1,40 @@
+require('minitest/rg')
+require('minitest/autorun')
+require_relative('../library.rb')
 
+class TestLibrary < MiniTest::Test
+
+  def setup()
+
+    @library = Library.new([
+    {
+      title: "lord_of_the_rings",
+      rental_details: {
+        student_name: "Jeff",
+        date: "01/12/16"
+      }
+    },
+    {
+      title: "to_kill_a_mockingbird",
+      rental_details: {
+        student_name: "Bill",
+        date: "01/09/16"
+      }
+    },
+    {
+      title: "of_mice_and_men",
+      rental_details: {
+        student_name: "Hannah",
+        date: "11/03/16"
+      }
+    }
+  ]
+  )
+  end
+
+  def test_get_books()
+    assert_equal(3, @library.get_books().count())
+  end
+
+
+end

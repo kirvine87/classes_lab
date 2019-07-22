41c44c44540847795d4af0aa8d41c84bee2fdbd7
Kyle <kirvine87@gmail.com>
2019-07-22 20:01:29 +0100

    completed get books, return all info, return rental info functions and tests

--- Extension/library.rb
+++ Extension/library.rb
@@ -8,4 +8,12 @@ def get_book()
   return @book_list
 end
 
+def return_all_info(student)
+  return student
+end
+
+def return_rental_info(student)
+  return student[:rental_details]
+end
+
 end
--- Extension/specs/library_spec.rb
+++ Extension/specs/library_spec.rb
@@ -5,21 +5,21 @@ require_relative('../library.rb')
 class TestLibrary < MiniTest::Test
 
 def setup()
-  @library = Library.new([{
+  @library = Library.new([@student = {
     title: "lord_of_the_rings",
     rental_details: {
      student_name: "Jeff",
      date: "01/12/16"
     }
   },
-  {
+  @student_2 = {
     title: "lord_of_the_flies",
     rental_details: {
      student_name: "Sandra",
      date: "22/09/01"
     }
   },
-  {
+  @student_3 = {
     title: "harry_potter",
     rental_details: {
      student_name: "Bob",
@@ -30,6 +30,14 @@ end
 
 def test_get_book()
   assert_equal(3, @library.get_book().count())
+end
+
+def test_return_all_info()
+  assert_equal(@student, @library.return_all_info(@student))
+end
+
+def test_return_rental_info()
+  assert_equal(@student[:rental_details], @library.return_rental_info(@student))
 end
 
 end

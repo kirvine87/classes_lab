7458abf2f8efa0126a7e6bbade40e4116c4aa77f
Kyle <kirvine87@gmail.com>
2019-07-23 10:54:37 +0100

    fixes to code as well as attempting the other functions

--- Extension/library.rb
+++ Extension/library.rb
@@ -1,5 +1,7 @@
 class Library
 
+  attr_reader :books
+
   def initialize(books)
     @books = books
   end
@@ -20,5 +22,23 @@ class Library
     book = get_book_by_title(title)
     return book[:rental_details]
   end
- 
+
+  # def add_new_book(title)
+  #   book = {
+  #     title: title,
+  #     rental_details: {
+  #       student_name: "",
+  #       date: ""
+  #     }
+  #   }
+  #   @books.push(book)
+  #
+  # end
+  #
+  # def update_details(name, student_name, date)
+  #   book_rental_details = rental_details_by_title(name)
+  #   book_rental_details[:student_name] = student_name
+  #   book_rental_details[:date] = date
+  #
+  # end
 end
--- Extension/specs/library_spec.rb
+++ Extension/specs/library_spec.rb
@@ -5,44 +5,47 @@ require_relative('../library.rb')
 class TestLibrary < MiniTest::Test
 
   def setup()
-
-    @library = Library.new([
-    {
-      title: "lord_of_the_rings",
-      rental_details: {
-        student_name: "Jeff",
-        date: "01/12/16"
+    books = [
+      {
+        title: "lord_of_the_rings",
+        rental_details: {
+          student_name: "Jeff",
+          date: "01/12/16"
+        }
+      },
+      {
+        title: "to_kill_a_mockingbird",
+        rental_details: {
+          student_name: "Bill",
+          date: "01/09/16"
+        }
+      },
+      {
+        title: "of_mice_and_men",
+        rental_details: {
+          student_name: "Hannah",
+          date: "11/03/16"
+        }
       }
-    },
-    {
-      title: "to_kill_a_mockingbird",
-      rental_details: {
-        student_name: "Bill",
-        date: "01/09/16"
-      }
-    },
-    {
-      title: "of_mice_and_men",
-      rental_details: {
-        student_name: "Hannah",
-        date: "11/03/16"
-      }
-    }
-  ]
-  )
+    ]
+    @library = Library.new(books)
   end
 
   def test_get_books()
     assert_equal(3, @library.get_books().count())
-  end 
+  end
 
-def test_get_book_by_title()
-  assert_equal(2, @library.get_book_by_title("to_kill_a_mockingbird").count())
-end
+  def test_get_book_by_title()
+    assert_equal(@library.books[1], @library.get_book_by_title("to_kill_a_mockingbird"))
+  end
 
-def test_get_rental_details_by_title()
-   rental_details = @library.rental_details_by_title('lord_of_the_rings')
-   assert_equal(2, rental_details.count())
- end
+  def test_get_rental_details_by_title()
+    rental_details = @library.rental_details_by_title('lord_of_the_rings')
+    assert_equal(@library.books[0][:rental_details], rental_details)
+  end
 
+  # def test_add_new_book
+  #   @library.add_new_book ('1984')
+  #   assert_equal('1984', @library.books[-1][:title])
+  # end
 end
